c38e0f3a3521671635f3a0be9c2c3029a755776f
Gustaf Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2021-08-04 14:46:14 +0200

    Header and Home components in place, installed material-ui for icons used, fixed image tag with airbnb logo, currently building the header component

---
+++ airbnb-homepage-clone/src/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Header.css';
+import airbnb_icon from '/Users/guscattokki/Documents/Gustaf/React/airbnb-home-page-clone/airbnb-homepage-clone/src/assets/airbnb_icon.png'
+import SearchIcon from '@material-ui/icons/Search';
+import LanguageIcon from '@material-ui/icons/Language';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Avatar } from "@material-ui/core"
+
+
+function Header() {
+    return (
+        <div className="header">
+            <img 
+                className="header__icon" 
+                src={airbnb_icon}
+                alt=""
+            /> 
+             <div className="header__center">
+                 <input type="text"></input>
+                    <SearchIcon />
+             </div>
+
+             <div className="header__right">
+                 <p>Become a host</p>
+                 <LanguageIcon />
+                 <ExpandMoreIcon />
+                 <Avatar />
+             </div>
+        </div>
+    )
+}
+
+export default Header
